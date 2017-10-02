2e91cb24a4f7cdb412e5bb5b796fcf3c29476a38
sirgippy <jegipson@gmail.com>
2017-10-01 20:26:19 -0500

    homepage logo and favicon

--- rcfbpoll/settings/production.py
+++ rcfbpoll/settings/production.py
@@ -1,6 +1,6 @@
 from rcfbpoll.settings.base import *
 
-DEBUG = True
+DEBUG = False
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
